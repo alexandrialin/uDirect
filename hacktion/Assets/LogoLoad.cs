dae1acc5c7dce3c47c65dc6c4396a729e71fe971
alexandrialin <alin92@uwo.ca>
2022-04-01 15:01:02 -0400

    first commit

---
+++ hacktion/Assets/LogoLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nextCanvas());
+        
+    }
+
+    public IEnumerator nextCanvas()
+    {
+        yield return new WaitForSeconds(3);
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ hacktion/Assets/MakeChoices.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeChoices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.CompareTag("")) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
