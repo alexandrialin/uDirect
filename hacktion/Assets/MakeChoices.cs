9a7025ecf398e3ac7f2b743613f10cb02c1f5ce3
alexandrialin <alin92@uwo.ca>
2022-04-01 20:17:07 -0400

    .

--- hacktion/Assets/MakeChoices.cs
+++ hacktion/Assets/MakeChoices.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 public class MakeChoices : MonoBehaviour
 {
     public static int characterType;
-    public static int petType;
-    public static int setting;
-    public static int colour;
-    public static int adjective;
-    public static int verb;
-    public static int food;
+    public static string petType;
+    public static string setting;
+    public static string colour;
+    public static string adjective;
+    public static string verb;
+    public static string food;
     public static int music;
     public Button yourButton;
     // Start is called before the first frame update
@@ -40,88 +40,88 @@ public class MakeChoices : MonoBehaviour
         }
         if (gameObject.CompareTag("p1"))
         {
-            petType = 1;
+            petType = "dog";
         }
         if (gameObject.CompareTag("p2"))
         {
-            petType = 2;
+            petType = "cat";
             print(petType);
         }
         if (gameObject.CompareTag("p3"))
         {
-            petType = 3;
+            petType = "pig";
         }
         if (gameObject.CompareTag("p4"))
         {
-            petType = 4;
+            petType = "panda";
         }
         if (gameObject.CompareTag("s1"))
         {
-            setting = 1;
+            setting = "outer space";
         }
         if (gameObject.CompareTag("s2"))
         {
-            setting = 2;
+            setting = "the beach";
         }
         if (gameObject.CompareTag("s3"))
         {
-            setting = 3;
+            setting = "the jungle";
         }
         if (gameObject.CompareTag("co1"))
         {
-            colour = 1;
+            colour = "blue";
         }
         if (gameObject.CompareTag("co2"))
         {
-            colour = 2;
+            colour = "red";
         }
         if (gameObject.CompareTag("co3"))
         {
-            colour = 3;
+            colour = "green";
         }
         if (gameObject.CompareTag("co4"))
         {
-            colour = 4;
+            colour = "yellow";
         }
         if (gameObject.CompareTag("a1"))
         {
-            adjective = 1;
+            adjective = "creepy";
         }
         if (gameObject.CompareTag("a2"))
         {
-            adjective = 2;
+            adjective = "itchy";
         }
         if (gameObject.CompareTag("a3"))
         {
-            adjective = 3;
+            adjective = "sassy";
         }
         if (gameObject.CompareTag("v1"))
         {
-            verb = 1;
+            verb = "walk";
         }
         if (gameObject.CompareTag("v2"))
         {
-            verb = 2;
+            verb = "run";
         }
         if (gameObject.CompareTag("v3"))
         {
-            verb = 3;
+            verb = "dance";
         }
         if (gameObject.CompareTag("f1"))
         {
-            food = 1;
+            food = "pizza";
         }
         if (gameObject.CompareTag("f2"))
         {
-            food = 2;
+            food = "burger";
         }
         if (gameObject.CompareTag("f3"))
         {
-            food = 3;
+            food = "spaghetti";
         }
         if (gameObject.CompareTag("f4"))
         {
-            food = 4;
+            food = "taco";
         }
         if (gameObject.CompareTag("m1"))
         {
@@ -146,27 +146,27 @@ public class MakeChoices : MonoBehaviour
     {
         return characterType;
     }
-    public static int pType()
+    public static string pType()
     {
         return petType;
     }
-    public static int sType()
+    public static string sType()
     {
         return setting;
     }
-    public static int colourType()
+    public static string colourType()
     {
         return colour;
     }
-    public static int adj()
+    public static string adj()
     {
         return adjective;
     }
-    public static int ver()
+    public static string ver()
     {
         return verb;
     }
-    public static int foodType()
+    public static string foodType()
     {
         return food;
     }
