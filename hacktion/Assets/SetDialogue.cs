717d928064b85028a7c7b3d5d0a39abffaaf0bcc
alexandrialin <alin92@uwo.ca>
2022-04-01 20:16:18 -0400

    .

---
+++ hacktion/Assets/SetDialogue.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetDialogue : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.CompareTag("t1"))
+        {
+            text.text = "In order to cook like " + SetName.getCharName() + "'s grandma, you must first adopt a pet " + MakeChoices.pType() + " and name it " + SetName.getPetName();
+        }
+        if (gameObject.CompareTag("t2"))
+        {
+            text.text = "Then, with voodoo you will be transported to " + MakeChoices.sType() + " where the skies smell like the ground and the ground tastes like the sky. ";
+        }
+        if (gameObject.CompareTag("t3"))
+        {
+            text.text = "Grandma is always watching, no matter where voodoo takes you.";
+        }
+        if (gameObject.CompareTag("t4"))
+        {
+            text.text = "Beware of the hot chili peppers, as they will certainly spice up your recipe to the point of no return and stain your kitchen walls " + MakeChoices.colourType() ;
+        }
+        if (gameObject.CompareTag("t5"))
+        {
+            text.text = "In case you weren't unaware, grandma likes her food " + MakeChoices.adj() + " and hot. ";
+        }
+        if (gameObject.CompareTag("t6"))
+        {
+            text.text = "Make sure to follow the recipe very closely and never stray far from any instructions, otherwise grandma will " + MakeChoices.ver() + " with disappointment.";
+        }
+        if (gameObject.CompareTag("t7"))
+        {
+            text.text = "If all goes well, everyone at the dinner table will develop a severe " + MakeChoices.foodType() + " addiction because of your ability to cook like "+ SetName.getCharName() + "’s grandma. ";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ hacktion/Assets/SpawnMC.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject char1;
+    public GameObject char2;
+    public GameObject char3;
+    public GameObject char4;
+    void Start()
+    {
+        if (gameObject.CompareTag("char"))
+        {
+            if (MakeChoices.charType() == 1)
+            {
+                char1.SetActive(true);
+            }
+            if (MakeChoices.charType() == 2)
+            {
+                char2.SetActive(true);
+            }
+            if (MakeChoices.charType() == 3)
+            {
+                char3.SetActive(true);
+            }
+            if (MakeChoices.charType() == 4)
+            {
+                char4.SetActive(true);
+            }
+        }
+        if (gameObject.CompareTag("pet"))
+        {
+            if (MakeChoices.pType() == "dog")
+            {
+                char1.SetActive(true);
+            }
+            if (MakeChoices.pType() == "cat")
+            {
+                char2.SetActive(true);
+            }
+            if (MakeChoices.pType() == "pig")
+            {
+                char3.SetActive(true);
+            }
+            if (MakeChoices.pType() == "panda")
+            {
+                char4.SetActive(true);
+            }
+        }
+        if (gameObject.CompareTag("colour"))
+        {
+            if (MakeChoices.colourType() == "blue")
+            {
+                char1.SetActive(true);
+            }
+            if (MakeChoices.colourType() == "red")
+            {
+                char2.SetActive(true);
+            }
+            if (MakeChoices.colourType() == "green")
+            {
+                char3.SetActive(true);
+            }
+            if (MakeChoices.colourType() == "yellow")
+            {
+                char4.SetActive(true);
+            }
+        }
+        if (gameObject.CompareTag("verb"))
+        {
+            if (MakeChoices.ver() == "walk")
+            {
+                char1.SetActive(true);
+            }
+            if (MakeChoices.ver() == "run")
+            {
+                char2.SetActive(true);
+            }
+            if (MakeChoices.ver() == "dance")
+            {
+                char3.SetActive(true);
+            }
+
+        }
+        if (gameObject.CompareTag("food"))
+        {
+            if (MakeChoices.colourType() == "pizza")
+            {
+                char1.SetActive(true);
+            }
+            if (MakeChoices.colourType() == "burger")
+            {
+                char2.SetActive(true);
+            }
+            if (MakeChoices.colourType() == "spaghetti")
+            {
+                char3.SetActive(true);
+            }
+            if (MakeChoices.colourType() == "taco")
+            {
+                char4.SetActive(true);
+            }
+        }
+        if (gameObject.CompareTag("music"))
+        {
+            if (MakeChoices.musicType() == 1)
+            {
+                Instantiate(char1);
+            }
+            if (MakeChoices.musicType() == 2)
+            {
+                Instantiate(char2);
+            }
+            if (MakeChoices.musicType() == 3)
+            {
+                Instantiate(char3);
+            }
+            if (MakeChoices.musicType() == 4)
+            {
+                Instantiate(char4);
+            }
+        }
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
